0f7f5534f1b5f9fca837c3cee59f61783caf81dd
khkk24 <franciscokn3@gmail.com>
2023-09-08 12:50:03 -0300

    melhorando a pagina product

--- src/components/organisms/Product/index.js
+++ src/components/organisms/Product/index.js
@@ -1,115 +1,74 @@
-import React, { useRef, useState } from 'react'
-import CardProduct from '../../molecules/CardProduct'
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
+import './styles.css';
+
+const CardProduct = () => {
+    return (
+        <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                </Card.Text>
+                <Card.Link href="#">Card Link</Card.Link>
+                <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+        </Card>
+    );
+};
+
+const Sidebar = ({ isSidebarOpen }) => {
+    return (
+        <Col xs={12} md={3} className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
+            {/* Contenu de la sidebar */}
+            <div className="sidebar-links">
+                <h4>Liens Rapides</h4>
+                <ul>
+                    <li>
+                        <a href="/page1">Lien vers Page 1</a>
+                    </li>
+                    <li>
+                        <a href="/page2">Lien vers Page 2</a>
+                    </li>
+                    <li>
+                        <a href="/page3">Lien vers Page 3</a>
+                    </li>
+                </ul>
+            </div>
+        </Col>
+    );
+};
 
 const Product = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
-
-    const scrollRef = useRef(null);
-    const [startIndex, setStartIndex] = useState(0);
-    
-    const cardsPerPage = 4;
-    const totalCards = 8;
-
-    const scrollLeft = () => {
-      
-        if (startIndex > 0) {
-            setStartIndex(startIndex - cardsPerPage);
-        }
-  if (scrollRef.current) {
-            scrollRef.current.scrollLeft -= 300;
-        }
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
     };
- 
-    const scrollRight = () => {
-       
-        if (startIndex + cardsPerPage < totalCards) {
-            setStartIndex(startIndex + cardsPerPage);
-        }
-        if (scrollRef.current) {
-            scrollRef.current.scrollRight += 300;
-        }
-    }
-    const cards = Array.from({ length: totalCards }, (_, index) => (
-        // <Col key={index} xs={12} md={6} lg={3}>
-            <CardProduct />
-        // </Col>
-    )).slice(startIndex, startIndex + cardsPerPage);
+
+    const cards = Array.from({ length: 12 }, (_, index) => <CardProduct key={index} />);
+
     return (
-//         // <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(18rem, 1fr))', gap: '1rem', padding: '1rem' }}>
-
-//         <div>
-//             <Row>
-                
-//                 <Col>
-//                     <div style={{
-//                         display: 'flex',
-//                         overflowX: 'scroll',
-//                         justifyContent: 'center',
-//                         alignItems: 'center',
-//                         height: '300px',
-//                         gap: '1rem',
-//                         padding: '1rem',
-//                         backgroundColor:'red'
-//                     }}
-//                     ref={scrollRef}
-//                     >
-//                         <button onClick={scrollLeft} disabled={startIndex === 0}>left</button>
-//                     {cards}
-//                 <button onClick={scrollRight} disabled={startIndex + cardsPerPage >= cardsPerPage.length}>right</button>
-//             </div>
-//                 </Col>
-                
-
-//             </Row>
-            
-        
-//         </div>
-//   )
-// }
-
         <Container fluid>
             <Row>
-                <Col xs={12}>
-                   
-               
-                {/* <Col xs={8}> */}
-                    <div
-                        style={{
-                            display: 'flex',
-                            // height: '400px',
-                        gap: '1rem',
-                            padding: '1rem',
-                            // overflowX: 'scroll',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                            // height: '300px',
-                        // backgroundColor:'red'
-                             overflowX: 'auto',
-                          
-                        }}
-                        ref={scrollRef}
-                    >
-                        <Button onClick={scrollLeft} disabled={startIndex === 0}>
-                        Left
-                    </Button>
+                <Sidebar isSidebarOpen={isSidebarOpen} />
+                <Col xs={12} md={9} className="custom-divider">
+                    {/* Contenu principal */}
+                    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(18rem, 1fr))', gap: '1rem', padding: '1rem' }}>
                         {cards}
-                        <Button
-                        onClick={scrollRight}
-                        // disabled={startIndex + cardsPerPage >= totalCards}
-                            disabled={startIndex === totalCards - 1}
-                    >
-                        Right
-                    </Button>
                     </div>
-                {/* </Col>
-                <Col xs={2} className="d-flex justify-content-end"> */}
-                    
                 </Col>
-            
-                
+                <Button
+                    variant="secondary"
+                    onClick={toggleSidebar}
+                    className={`toggle-button d-md-none ${isSidebarOpen ? 'open' : ''}`}
+                >
+                    {isSidebarOpen ? 'Fermer' : 'Ouvrir'}
+                </Button>
             </Row>
         </Container>
     );
 };
-export default Product
+
+export default Product;
